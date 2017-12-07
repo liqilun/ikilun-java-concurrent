deaff0adbad1e319f05de777360c57c45ee27aa5
李启仑 <lun.li@gewara.com>
2017-12-07 16:32:18 +0800

    ThreadPoolExecutor

--- src/main/java/com/ikilun/ThreadPoolExecutorTest.java
+++ src/main/java/com/ikilun/ThreadPoolExecutorTest.java
@@ -1,7 +1,21 @@
 package com.ikilun;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadPoolExecutorTest {
+	private static BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(5);
+	private static ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 0, TimeUnit.SECONDS, workQueue);
 	public static void main(String[] args) {
-		
+		executor.allowCoreThreadTimeOut(false);
+		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				
+			}
+		});
 	}
 }
